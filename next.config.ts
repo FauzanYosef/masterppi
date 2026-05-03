9cd61045e5d287fe22db466f4a0b66eb63ad15bf
Fauzan Anwar <fauzanyosef27@gmail.com>
2026-05-03 11:57:35 +0700

    change setting config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ❌ HAPUS ini
-  // output: "export",
-
-  // ❌ sementara hapus dulu
-  // basePath: "/MasterPPI",
-  // assetPrefix: "/MasterPPI",
-
   images: {
-    domains: ['vfiikiikssljzqrkervl.supabase.co'],
-  },
-
-  trailingSlash: true,
-
-  eslint: {
-    ignoreDuringBuilds: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
   },
 };
 
